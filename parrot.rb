67983e3a14001cc2e03a02ad6792603db52a934b
Chanda (Cat) Grayson <chandagrayson@gmail.com>
2018-11-10 18:08:09 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = "Squawk!")
-  puts "#{phrase}"
+def parrot(phrase)
+  puts "Squawk! #{phrase}"
 end
 
-parrot("Pretty Bird!")
+parrot
